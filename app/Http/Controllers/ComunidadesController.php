e1010b6fe31ee76e34fc57f3a89e60d183bc084a
aliinerocha <aliine.rocha@hotmail.com>
2020-01-17 17:27:54 -0300

    primeiro mcv comunidades

---
+++ app/Http/Controllers/ComunidadesController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ComunidadesController extends Controller
+{
+    public function show($id) {
+        return view('comunidades.show', compact('id'));
+    }
+}
