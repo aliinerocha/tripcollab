e5b1c5f46d1091f52dedcf395991e51b9e69a5e9
rose-yumi <roseane.watanabe@gmail.com>
2020-02-19 16:32:57 -0300

    Revisado páginas das Comunidades

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::group(['middleware' => ['auth']], function () {
 
     Route::get('trip/{id}', "Trip\TripController@show")->name('trip.show');
 
-    Route::get('trip/create', "Trip\TripController@create")->name('trip.create');
+    Route::get('trips/create', "Trip\TripController@create")->name('trip.create');
 
     Route::post('trip/store', "Trip\TripController@store")->name('trip.store');
 
@@ -43,8 +43,7 @@ Route::group(['middleware' => ['auth']], function () {
 
     Route::put('/profile/{id}' , 'User\UserController@update')->name('update');
 
-    // Inicio das Rotas das Comunidades
-    // Route::get('index/comunidadeEViagens', 'Group\GroupController@index', 'Trip\TripController@index')->name('index');
+    // Groups
 
     Route::get('group/create', 'Group\GroupController@create')->name('group.create');
 
@@ -58,9 +57,7 @@ Route::group(['middleware' => ['auth']], function () {
 
     Route::delete('/group/{id}', "Group\GroupController@destroy")->name('group.destroy');
 
-    // Fim das Rotas das Comunidades
-
-    //Inicio das Rotas dos Topicos
+    // Topics
 
     Route::get('topic/create', 'Group\TopicController@create')->name('topic.create');
 
@@ -74,7 +71,6 @@ Route::group(['middleware' => ['auth']], function () {
 
     Route::delete('/topic/{id}', "Group\TopicController@destroy")->name('topic.destroy');
 
-    //Fim das Rotas dos Topicos
 });
 
 /*
